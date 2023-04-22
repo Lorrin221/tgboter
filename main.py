486bd031239e46dcdf4416b59d42b224aea1593f
Lorrin221 <typovar24@mail.ru>
2023-04-22 20:51:15 +0300

    tgbot

--- main.py
+++ main.py
@@ -31,19 +31,21 @@ async def start(update, context):
 
 async def loginner(update, context):
     await update.message.reply_text("Пожалуйста, отправьте ваш Яндекс_ID:")
-    return 3
+    return 1
 
 
 async def signupper(update, context):
-    await update.message.reply_text("Пожалуйста, отправьте ваш Яндекс_ID:")
-    return 4
+    await update.message.reply_text("Пожалуйста, отправьте ваши данные в таком порядке(через пробел):"
+                                    "Логин, Яндекс_ID, Имя, Фамилия")
+    return 1
 
 
-async def login(update):
+async def login(update, context):
+    global body
     user_id = update.message.from_user.id
     update.message.reply_text(f'Пожалуйста, отправьте ваш Яндекс_ID:', reply_markup=ReplyKeyboardRemove())
     res = cur.execute(f"SELECT first_name, last_name FROM accounts"
-                      f"WHERE yandex_id == {user_id}").fetchall()
+                      f"WHERE yandex_id={user_id}").fetchall()
     if len(res) == 0:
         await update.message.reply_text(f'Простите, но аккаунта с таким id не существует.')
     else:
@@ -53,25 +55,34 @@ async def login(update):
 
 
 async def signup(update, context):
-    user_id = update.message.text
-    res = cur.execute(f'SELECT account_id FROM teachers WHERE account_id == {user_id}').fetchall()
+    login, yandex_id, f_name, l_name = update.message.text.split()
+    res = cur.execute(f'SELECT yandex_id FROM accounts WHERE yandex_id = {yandex_id}').fetchall()
     if len(res) != 0:
         await update.message.reply_text(f'Простите, но аккаунт с таким id уже существует.')
     else:
         await update.message.reply_text(f'Добро пожаловать в наш школьный мир!')
+        cur.execute(f'INSERT INTO accounts("login, yandex_id, acctype, first_name, last_name") '
+                    f'VALUES({login}, {yandex_id}, 0, {f_name}, {l_name})')
+        con.commit()
 
 
 async def exitor(update, context):
+    global body
     await update.message.reply_text("Вы вышли из своего аккаунта. До свидания!")
     body = 0
     return ConversationHandler.END
 
 
 async def send(update, context):
-    await update.message.reply_text(
-        "Введите имя и фамилию отправителя, первая буква имени и первая буква фамилии ЗАГЛАВНАЯ,"
-        "все остальные символы - строчные буквы, иных символов быть не должно")
-    return 1
+    if body == 1:
+        await update.message.reply_text(
+            "Введите имя и фамилию отправителя, первая буква имени и первая буква фамилии ЗАГЛАВНАЯ,"
+            "все остальные символы - строчные буквы, иных символов быть не должно")
+        return 1
+    else:
+        await update.message.reply_text(
+            "Для начала войдите в аккаунт")
+        return ConversationHandler.END
 
 
 async def idir(update, context):
@@ -96,22 +107,26 @@ async def is_sent(update, context):
 
 def main():
     application = Application.builder().token('6232251709:AAFyEC6d0nKHHnW9ICwmUXJcLRqPocyeffo').build()
-    conv_handler = ConversationHandler(
-        # Точка входа в диалог.
-        # В данном случае — команда /start. Она задаёт первый вопрос.
-        entry_points=[CommandHandler('start', start)],
-
-        # Состояние внутри диалога.
-        # Вариант с двумя обработчиками, фильтрующими текстовые сообщения.
+    log = ConversationHandler(
+        entry_points=[CommandHandler('login', loginner)],
         states={
-            # Функция читает ответ на первый вопрос и задаёт второй.
-            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, login)],
-            # Функция читает ответ на второй вопрос и завершает диалог.
-            2: [MessageHandler(filters.TEXT & ~filters.COMMAND, signup)],
-            3: [MessageHandler(filters.TEXT & ~filters.COMMAND, loginner)],
-            4: [MessageHandler(filters.TEXT & ~filters.COMMAND, signupper)]
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, login)]
         },
-
+        fallbacks=[CommandHandler('exit', exitor)]
+    )
+    sig = ConversationHandler(
+        entry_points=[CommandHandler('signup', signupper)],
+        states={
+            1: [MessageHandler(filters.TEXT & ~filters.COMMAND, signup)]
+        },
+        fallbacks=[CommandHandler('exit', exitor)]
+    )
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            1: [CommandHandler('signup', signupper)],
+            2: [CommandHandler('login', loginner)]
+        },
         fallbacks=[CommandHandler('exit', exitor)]
     )
     sender = ConversationHandler(
@@ -122,10 +137,12 @@ def main():
         },
         fallbacks=[CommandHandler('exit', is_sent)]
     )
+    application.add_handler(log)
+    application.add_handler(sig)
     application.add_handler(conv_handler)
     application.add_handler(sender)
-    application.add_handler(CommandHandler("login", loginner))
-    application.add_handler(CommandHandler("signup", signupper))
+    application.add_handler(CommandHandler('login', loginner))
+    application.add_handler(CommandHandler('signup', signupper))
 
     application.run_polling()
 
